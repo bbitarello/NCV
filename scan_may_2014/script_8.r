aa0365c022dcbc82ea1b5ddae9937a7c32b927fa
BARBARA BITARELLO <barbara@usp.br>
2014-10-30 20:22:58 +0100

    updating scripts

---
+++ scan_may_2014/script_8.r
@@ -0,0 +1,13 @@
+####################################################################################################################################################
+#	Barbara D Bitarello	
+#
+#	Considering I'v eplayed around with the data, in this script I will try to do the 'real' analyses, the ones I intend to use for the paper
+#
+#
+#	Last modified
+#####################################################################################################################################################
+
+
+
+#Remember, in script_1.r I read in the data.
+
