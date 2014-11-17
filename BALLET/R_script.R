3f249e516cecee5b1d978acef17015df4c3e1555
BARBARA BITARELLO <barbara@usp.br>
2014-11-17 14:40:45 +0100

    ROC again

--- BALLET/R_script.R
+++ BALLET/R_script.R
@@ -27,3 +27,32 @@ plot(perf, col='red', lty=2)
 dev.off()
 
 vioplot(bs$V1, neu$V1, names=c('bs', 'neu'), ylim=c(min(neu$V1),max(bs$V1)))
+
+
+a<-sort(T2_neu$V1)
+b<-sort(T2_bs$V1)
+my.ROC<-function(x,y){
+sort(x)->x
+sort(y)->y
+p<-x
+n<-y
+seq(from=0,to=1, 0.01)->s
+
+tp<-rep(NA,100)
+fp<-rep(NA,100)
+fn<-rep(NA,100)
+tn<-rep(NA,100)
+for (i in 1:length(s)){
+s[i]*1000->temp
+sum(p>=n[temp])->tp[i]
+fn=1000-fp
+fp<-(1-s)*1000
+tn<-1000-fp
+}
+tpr<-(tp/(tp+fn))
+fpr<-(fp/(tn+fp))
+res<-list(TPR=tpr, FPR=fpr,TP=tp, TN=tn, FP=fp, FN=tn, Q=s)
+return(res)
+}
+my.ROC(b,a) #currently not working so well
+
