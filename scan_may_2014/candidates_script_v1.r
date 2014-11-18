a3223c490cc074f117014f66fe19993002545c1b
BARBARA BITARELLO <barbara@usp.br>
2014-11-18 20:23:17 +0100

    18.11.2014

--- scan_may_2014/candidates_script_v1.r
+++ scan_may_2014/candidates_script_v1.r
@@ -19,6 +19,8 @@ library(ggplot2)
 
 load('/mnt/sequencedb/PopGen/barbara/scan_may_2014/All.Results.Final.RData')
 
+
+##########################skip the next block, as it has already been saved in the R object ############################
 pops<-c("AWS","LWK","YRI","CEU", "FIN","GBR","TSI", "CHB","CHS" ,"JPT","MXL", "CLM","PUR")
 
 names(All.Results.Final)<-pops
@@ -51,40 +53,60 @@ lapply(All.Results.Final, function(x) cbind(x,Nr.IS=(x$Nr.SNPs+x$Nr.FDs)))-> All
 #now we have a data frame which contain the Nr.US
 
 
-lapply(All.Res, function(x) cbind(x,PtoD=x$Nr.SNPs/(x$Nr.FDs+1)))-> All.Res
+lapply(All.Res, function(x) cbind(x,PtoD=x$Nr.SNPs/(x$Nr.FDs+1)))-> All.Res1
 
 
-lapply(All.Res, function(x) dim(x)) #dimensions of initial data
+lapply(All.Res1, function(x) dim(x)) #dimensions of initial data
+
+
+
 # 1705970
-lapply(All.Res, function(x) subset(x, Nr.IS>=4))->All.Res.4.IS
+lapply(All.Res1, function(x) subset(x, Nr.IS>=4))->All.Res.4.IS
 #yri:1698893
-lapply(All.Res.4.IS, function(x) cbind(x, P.val.bin=rep(NA, dim(x)[1])))->All.Res.4.IS
-
-lapply(All.Res.4.IS, function(x) dim(x)) #dimensions of data with >=4 I.S
+lapply(All.Res.4.IS, function(x) cbind(x, P.val.NCVf0.5=rep(NA, dim(x)[1]), P.val.NCVf0.4=rep(NA, dim(x)[1]), P.val.NCVf0.3=rep(NA, dim(x)[1]), P.val.NCVf0.2=rep(NA, dim(x)[1]), P.val.NCVf0.1=rep(NA, dim(x)[1])))->All.Res.4.IS
 
 lapply(All.Res.4.IS, function(x) subset(x, Proportion.Covered>=0.5))-> All.Res.4.IS.prop50
+#dim YRI:1627870 
 
-#YRI
-#1627870      16
+objectName<-'All.Res.4.IS.prop50'
 
-
+save(list=objectName, file= 'All.Res.4.IS.prop50.RData')    
 #######################################################################################################################
 ##Second part###########
 ########################
 ##Read in simulations which encompass the rang eof informative sites####
 ########################
 ########################################################################################################################
+library(parallel)
+
+list.MSMS.rec.1e_09<-mclapply(c(0:5,"5b",6), function(x) read.table(paste0("/mnt/sequencedb/PopGen/cesare/bs_genomescan/simulations/msms/neutral_n100_mu1e-07_rho1e-09_3000bp.downsampled.allstats",x,".tsv.gz"), header=T))
+
 
 list.MSMS<-vector('list', 3)
 
-read.table(gzfile('/mnt/sequencedb/PopGen/cesare/bs_genomescan/simulations/msms/neutral_n100_mu1e-07_rho1e-09_3000bp.downsampled.allstats6.tsv.gz'), header=T)-> list.MSMS[[1]]
-read.table(gzfile('/mnt/sequencedb/PopGen/cesare/bs_genomescan/simulations/msms/neutral_n100_mu1e-07_rho1e-08_3000bp.downsampled.allstats6.tsv.gz'), header=T)-> list.MSMS[[2]]
-read.table(gzfile('/mnt/sequencedb/PopGen/cesare/bs_genomescan/simulations/msms/neutral_n100_mu1e-07_rho1e-07_3000bp.downsampled.allstats6.tsv.gz'), header=T)-> list.MSMS[[3]]
+do.call('rbind', list.MSMS.rec.1e_09)-> list.MSMS[[1]]
 
-names(list.MSMS)<-c('rec.1e_09', 'rec.1e_08', 'rec.1e_07')
+remove(list.MSMS.rec.1e_09)
+
+
+Store(list.MSMS)
+#
+list.MSMS.rec.1e_08<-mclapply(c(0:5,"5b",6), function(x) read.table(paste0("/mnt/sequencedb/PopGen/cesare/bs_genomescan/simulations/msms/neutral_n100_mu1e-07_rho1e-08_3000bp.downsampled.allstats",x,".tsv.gz"), header=T))
+list.MSMS.rec.1e_07<-mclapply(c(0:5,"5b",6), function(x) read.table(paste0("/mnt/sequencedb/PopGen/cesare/bs_genomescan/simulations/msms/neutral_n100_mu1e-07_rho1e-07_3000bp.downsampled.allstats",x,".tsv.gz"), header=T))
+
+
+do.call('rbind', list.MSMS.rec.1e_08)-> list.MSMS[[2]]
+do.call('rbind', list.MSMS.rec.1e_07)-> list.MSMS[[3]]
+
+
+remove(list.MSMS.rec.1e_08)
+remove(list.MSMS.rec.1e_07)
+
+#
 
 lapply(list.MSMS, function(x) cbind(x, Nr.IS=x$S+x$FD))-> list.MSMS
 #first separate AF< EU, AS
+names(list.MSMS)<-c('rec.1e_09', 'rec.1e_08', 'rec.1e_07')
 
 AFRICA<-vector('list', 3)
 names(AFRICA)<-c('rec.1e_09', 'rec.1e_08', 'rec.1e_07')
@@ -97,14 +119,14 @@ AFRICA[[1]]<-subset(list.MSMS[[1]], pop==0)
 AFRICA[[2]]<-subset(list.MSMS[[2]], pop==0)
 AFRICA[[3]]<-subset(list.MSMS[[3]], pop==0)
 
-#EUROPE[[1]]<-subset(list.MSMS[[1]], pop==1)
-#EUROPE[[2]]<-subset(list.MSMS[[2]], pop==1)
-#EUROPE[[3]]<-subset(list.MSMS[[3]], pop==1)
+EUROPE[[1]]<-subset(list.MSMS[[1]], pop==1)
+EUROPE[[2]]<-subset(list.MSMS[[2]], pop==1)
+EUROPE[[3]]<-subset(list.MSMS[[3]], pop==1)
 
 
-#ASIA[[1]]<-subset(list.MSMS[[1]], pop==2)
-#ASIA[[2]]<-subset(list.MSMS[[2]], pop==2)
-#ASIA[[3]]<-subset(list.MSMS[[3]], pop==2)
+ASIA[[1]]<-subset(list.MSMS[[1]], pop==2)
+ASIA[[2]]<-subset(list.MSMS[[2]], pop==2)
+ASIA[[3]]<-subset(list.MSMS[[3]], pop==2)
 #separate sims in bins of Nr.Inf. SItes
 
 my.thr<-function(x, y){
@@ -122,33 +144,89 @@ return(df)}
 
 lapply(AFRICA, function(x) my.thr(x,0.001))-> res.thr
 
-for ( i in 1:3){
+system.time(for ( i in 1:3){
 write.table(res.thr[[i]], file=paste0('thr.neutral.', names(AFRICA)[[i]]), row.names=F, col.names=T, sep="\t")
-}
+})
 
 
 #x: data
 #y: thr
 
+temp<-seq(from=4, to=496)
 
-#artifically introduce thr for BR.IS>352 in the above data.frame, and then run the for loop below.
+temp2<-as.data.frame(cbind(thr=rep(NA, length(temp)), bin.IS=temp, Nr.cases=rep(NA, length(temp))))
 
-list.thr.data<-vector('list', dim(res.thr[[2]])[1])
-
+#tmp.thr2<-dataframe(thr=rep(0.48, length(temp)), bin.IS=temp, Nr.cases=rep(0, length(temp)))
+ 
 
 for (i in 1: dim(res.thr[[2]])[1]){
 
+res.thr[[2]][i,2]->a
+
+which(temp2$bin.IS==a)-> a1
+
+temp2[a1,1]<-res.thr[[2]][i,1]
+temp2[a1,2]<-a
+temp2[a1,3]<-res.thr[[2]][i,3]
+}
+#artifically introduce thr for BR.IS>352 in the above data.frame, and then run the for loop below.
+
+list.thr.data<-vector('list', dim(res.thr[[2]])[1]+1)
+
+
+system.time(for (i in 1: dim(res.thr[[2]])[1]){
+
 subset(x, Nr.IS==res.thr[[2]][i,2])-> list.thr.data[[i]]
 
-}
+})
+
+
+list.thr.data[[dim(res.thr[[2]])[1]]]<-subset(x, Nr.IS>res.thr[[2]][dim(res.thr[[2]])[1],2])
+
+
+
 
 #now apply p-values to each element of the list based on the simulations. It should be faster than reading in the entire genomic scans at once.
 
 
 
+#read in NCV cutoff
+
+list.cutoffs<-vector('list', 3)
+
+
+list.cutoffs[[1]]<-read.table('/mnt/sequencedb/PopGen/cesare/bs_genomescan/simulations/msms/ncv_cutoffs_r1e-09_africa.tsv', header=T)
+list.cutoffs[[2]]<-read.table('/mnt/sequencedb/PopGen/cesare/bs_genomescan/simulations/msms/ncv_cutoffs_r1e-08_africa.tsv', header=T)
+list.cutoffs[[3]]<-read.table('/mnt/sequencedb/PopGen/cesare/bs_genomescan/simulations/msms/ncv_cutoffs_r1e-07_africa.tsv', header=T)
+
+names(list.cutoffs)<-c('rec.1e_09', 'rec.1e_08', 'rec.1e_07')
+
+a<-data.frame(nSites=seq(from=202, to=496),X1.=rep(list.cutoffs[[2]][201,2], 295)     , X0.1.= rep(list.cutoffs[[2]][201,3], 295)    ,X0.05.= rep(list.cutoffs[[2]][201,4], 295)   ,X0.1.Sub=rep(list.cutoffs[[2]][201,5], 295) , minSub=rep(list.cutoffs[[2]][201,6], 295) )  
+
+rbind(list.cutoffs[[2]], a)-> cutoffs.rec.1e_08
 
 
 
+list.thr.data<-vector('list', 202)
 
+
+system.time(for (i in 1: 201){
+
+subset(x, Nr.IS==cutoffs.rec.1e_08[i,1])-> list.thr.data[[i]]
+
+})
+
+list.thr.data[[202]]<-subset(x, Nr.IS>201)
+
+for (i in 1: dim(list.thr.data)[[1]]){
+
+list.thr.data[[i]][which(list.thr.data[[i]]$NCVf5<=cutoffs.rec.1e_08[i,6]),16]<-'PASS'
+list.thr.data[[i]][-which(list.thr.data[[i]]$NCVf5<=cutoffs.rec.1e_08[i,6]),16]<-'NOTPASS'
+
+
+
+dim(do.call(rbind, list.thr.data)) #just check if the dim is the same as x, because it should be.
+
+#NExt, add information if a windows passes a certain threshold or not. (from the cutoffs.rec object)
 
 
--- scan_may_2014/script_8.r
+++ scan_may_2014/script_8.r
@@ -9,5 +9,192 @@
 
 
 
-#Remember, in script_1.r I read in the data.
+#Remember, in script_1.r I read in the data and save as RData
+
+
+
+#############################################
+library(parallel)
+library(SOAR)  #speed up workspace loading
+library(ggplot2)
+######################################################################################
+######################################################################################
+#PArt II: Load balancing selection candidate genes
+pops<-c("AWS","LWK","YRI","CEU", "FIN","GBR","TSI", "CHB","CHS" ,"JPT","MXL", "CLM","PUR")
+#load results for scan without FDs
+
+load('/mnt/sequencedb/PopGen/barbara/scan_may_2014/All.Results.Final.no.FD.RData')
+
+load('/mnt/sequencedb/PopGen/barbara/scan_may_2014/All.Results.Final.RData')
+
+
+
+andres_2009<- read.table('andres.2009.bed')
+
+DG_genes<-read.table('DG.2014.bed')
+
+mhc.coords<-read.table('mhc.coords.gencode.bed')
+
+pseudogenes<-read.table('pseudogenes.bed')
+
+#MHC coordinates (by Deborah, but remember that this differs a bit from GENCODE v.19...it is just a quick way to check for HLA windows, but I will remove it after I have my own bedfile for HLA made from GENCODE directly.
+#read.table('mhc_shiina_hg19.bed', header=F)-> mhc.coords
+
+names(mhc.coords.gencode)<-c('chr','B', 'E', 'Name')
+
+names(andres_2009)<-c('chr','B', 'E', 'Name')
+
+names(DG_genes)<-c('chr','B', 'E', 'Name')
+
+names(pseudogenes)<-c('chr', 'B', 'E', 'Name')
+
+#####################################################################################################
+
+listA<-vector('list', 5)
+
+
+bed.names<-c('genes.m.YRItop.f5.bed', 'genes.m.YRItop.f5.min4SNPs.bed', 'genes.m.YRItop.f5.noFD.bed', 'genes.m.YRItop.f5.noFD.min4SNPs.bed')
+
+names(listA)<-bed.names
+
+
+
+bed.header<-c('chr', 'beg.pos.scan', 'end.pos.scan', 'wind.ID', 'chrb','beg.pos.genc', 'end.pos.genc', 'gene.name', 'type', 'gene_id' , 'type.2', 'overlap')
+
+
+for (i in 1:length(bed.names)){
+
+read.table(bed.names[i])-> listA[[i]]
+
+names(listA[[i]])<-bed.header
+
+}
+
+
+#########################################
+
+
+
+
+
+
+#go to script_5.r
+
+
+
+
+All.Results.Final[[3]]->YRI.with.FD
+
+All.Results.Final.no.FD[[3]]->YRI.no.FD
+
+
+remove(All.Results.Final)
+remove(All.Results.Final.no.FD)
+
+#input #FDs for the NCV-no-FD dataset.
+
+YRI.with.FD$Nr.FDs-> YRI.no.FD$Nr.FDs
+
+cbind(YRI.with.FD, as.data.frame(YRI.with.FD$Nr.SNPs/(YRI.with.FD$Nr.FDs+1)))->YRI.with.FD #avoid infinite values and keeping the ratio of P to D
+
+cbind(YRI.no.FD, as.data.frame(YRI.no.FD$Nr.SNPs/(YRI.no.FD$Nr.FDs+1)))->YRI.no.FD
+
+colnames(YRI.with.FD)[14]<-"PtoD"
+
+colnames(YRI.no.FD)[14]<-"PtoD"
+
+subset(YRI.with.FD, Proportion.Covered>=0.5)-> YRI.with.FD.prop50
+
+subset(YRI.with.FD, Nr.SNPs+Nr.FDs>=2)-> YRI.with.FD.2.IS
+
+subset(YRI.with.FD, Nr.SNPs+Nr.FDs>=3)-> YRI.with.FD.3.IS
+
+subset(YRI.with.FD, Nr.SNPs+Nr.FDs>=4)-> YRI.with.FD.4.IS
+
+subset(YRI.with.FD, Nr.SNPs+Nr.FDs>=5)-> YRI.with.FD.5.IS
+
+subset(YRI.with.FD, Nr.SNPs>=4)-> YRI.with.FD.4.SNPs
+
+subset(YRI.with.FD.4.IS, Proportion.Covered>=0.5)-> YRI.with.FD.prop50.4.IS
+
+YRI.with.FD[with(YRI.with.FD, order(NCVf5)), ]->sort.YRI.with.FD
+
+YRI.with.FD.2.IS[with(YRI.with.FD.2.IS, order(NCVf5)), ]->sort.YRI.with.FD.2.IS
+
+YRI.with.FD.3.IS[with(YRI.with.FD.3.IS, order(NCVf5)), ]->sort.YRI.with.FD.3.IS
+
+YRI.with.FD.4.IS[with(YRI.with.FD.4.IS, order(NCVf5)), ]->sort.YRI.with.FD.4.IS
+
+YRI.with.FD.5.IS[with(YRI.with.FD.5.IS, order(NCVf5)), ]->sort.YRI.with.FD.5.IS
+
+YRI.with.FD.4.SNPs[with(YRI.with.FD.4.SNPs, order(NCVf5)),]-> sort.YRI.with.FD.4.SNPs
+
+YRI.with.FD.prop50[with(YRI.with.FD.prop50, order(NCVf5)), ]->sort.YRI.with.FD.prop50
+
+YRI.with.FD.prop50.4.IS[with(YRI.with.FD.prop50.4.IS, order(NCVf5)), ]->sort.YRI.with.FD.prop50.4.IS
+
+listA<-vector('list', 8)
+listA[[1]]<-sort.YRI.with.FD
+listA[[2]]<-sort.YRI.with.FD.2.IS
+listA[[3]]<-sort.YRI.with.FD.3.IS
+listA[[4]]<-sort.YRI.with.FD.4.IS
+listA[[5]]<-sort.YRI.with.FD.5.IS
+listA[[6]]<-sort.YRI.with.FD.4.SNPs
+listA[[7]]<-sort.YRI.with.FD.prop50
+listA[[8]]<-sort.YRI.with.FD.prop50.4.IS
+
+#like this it is easier to apply functions to all datasets.
+
+names(listA)<-c("No.filters", "2 IS", "3 IS", "4 IS" , "5 IS", "4 SNPs", "Cov>=0.5", "4 IS & Cov>=0.5")
+
+
+# % OF fdS==0
+#(unlist(lapply(listB, function(x) table(x$Nr.FDs==0)[[2]]))/unlist(lapply(listB, function(x) dim(x)[[1]])))*100
+
+p.val2<-(seq(1:dim(listA[[8]])[[1]]))/(dim(listA[[8]])[1])
+
+
+cbind(listA[[8]], P.val=p.val2)->listB
+
+cbind(listB, Bin.IS=rep(NA,dim(listB)[1]))-> test
+cbind(test, P.val.bin=rep(NA, dim(listB)[1]))->final.dat
+cbind(final.dat, Nr.IS=final.dat$Nr.SNPs+final.dat$Nr.FDs)->final.dat
+
+
+
+
+
+subset(final.dat, Nr.IS<=39)-> final.dat1
+subset(final.dat, Nr.IS>39 & Nr.IS<52)->final.dat2
+subset(final.dat, Nr.IS>=52)->final.dat3
+
+final.dat1$Bin.IS<-rep(1, dim(final.dat1)[1])
+final.dat2$Bin.IS<-rep(2, dim(final.dat2)[1])
+
+final.dat3$Bin.IS<-rep(3, dim(final.dat3)[1])
+
+rbind(final.dat1, final.dat2, final.dat3)-> FINAL.DAT
+
+
+FINAL.DAT[with(FINAL.DAT, order(NCVf5)), ]->sort.FINAL.DAT
+
+
+my.bin.based.candidates<-cbind(subset(final.dat1, NCVf5<=0.3736396), subset(final.dat2, NCVf5<=0.4378912), subset(final.dat3, NCVf5<=0.4268826 ))
+
+
+
+read.table('topf5.YRI.bed') -> my.candidates
+
+bed.header<-c('chr', 'beg.pos.scan', 'end.pos.scan', 'wind.ID', 'chrb','beg.pos.genc', 'end.pos.genc', 'gene.name', 'type', 'gene_id' , 'type.2', 'overlap')
+
+names(my.candidates)<-bed.header
+
+my.candidates[,-9]-> my.candidates
+
+
+
+
+
+as.character(unique(subset(my.candidates, type.2=='protein_coding' & overlap>=1000)$gene.name))-> listD  #1206 genes
+
 
