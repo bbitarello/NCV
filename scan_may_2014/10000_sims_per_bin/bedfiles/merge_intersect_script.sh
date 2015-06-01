509282f8c157239db389f2ea707f2c43b3620fe0
BARBARA BITARELLO <barbara@usp.br>
2015-06-01 07:21:05 +0200

    GO bed files

--- scan_may_2014/10000_sims_per_bin/bedfiles/merge_intersect_script.sh
+++ scan_may_2014/10000_sims_per_bin/bedfiles/merge_intersect_script.sh
@@ -1,7 +1,7 @@
 
 ##################################################
 #Barbara D Bitarello
-#Modified:17.5.2015
+#Modified:18.5.2015
 ###################################################
 
 while read i;
@@ -16,12 +16,12 @@ mergeBed -i $i.top816.f0.4.bed -nms| perl -pe 's/ +/ /g'|perl -pe 's/ /\t/g'| se
 mergeBed -i $i.top816.f0.3.bed -nms| perl -pe 's/ +/ /g'|perl -pe 's/ /\t/g'| sed 's/^/chr/'  > m.$i.top816.f0.3.bed
 
 #intersect top100
-bedtools intersect -wo -a m.$i.top100.f0.5.bed -b ../final_encode.bed > intsc.$i.top100.f0.5.bed
-bedtools intersect -wo -a m.$i.top100.f0.4.bed -b ../final_encode.bed > intsc.$i.top100.f0.4.bed
-bedtools intersect -wo -a m.$i.top100.f0.3.bed -b ../final_encode.bed > intsc.$i.top100.f0.3.bed
+bedtools intersect -wo -a m.$i.top100.f0.5.bed -b ../ensembl_hg19.bed > intsc.$i.top100.f0.5.bed
+bedtools intersect -wo -a m.$i.top100.f0.4.bed -b ../ensembl_hg19.bed > intsc.$i.top100.f0.4.bed
+bedtools intersect -wo -a m.$i.top100.f0.3.bed -b ../ensembl_hg19.bed > intsc.$i.top100.f0.3.bed
 
 #intersect top816
-bedtools intersect -wo -a m.$i.top816.f0.5.bed -b ../final_encode.bed > intsc.$i.top816.f0.5.bed
-bedtools intersect -wo -a m.$i.top816.f0.4.bed -b ../final_encode.bed > intsc.$i.top816.f0.4.bed
-bedtools intersect -wo -a m.$i.top816.f0.3.bed -b ../final_encode.bed > intsc.$i.top816.f0.3.bed
+bedtools intersect -wo -a m.$i.top816.f0.5.bed -b ../ensembl_hg19.bed > intsc.$i.top816.f0.5.bed
+bedtools intersect -wo -a m.$i.top816.f0.4.bed -b ../ensembl_hg19.bed > intsc.$i.top816.f0.4.bed
+bedtools intersect -wo -a m.$i.top816.f0.3.bed -b ../ensembl_hg19.bed > intsc.$i.top816.f0.3.bed
 done < pops.list.txt
